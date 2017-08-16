11a6999b4468579d5e8573ebea6dc5dcdce3cc1f
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-17 02:12:17 +0300

    update class PhotoGalleryActivity

--- app/src/main/java/com/vg/photogallery/controller/activities/PhotoGalleryActivity.java
+++ app/src/main/java/com/vg/photogallery/controller/activities/PhotoGalleryActivity.java
@@ -1,10 +1,16 @@
 package com.vg.photogallery.controller.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import com.vg.photogallery.controller.activities.templates.SingleFragmentActivity;
 import com.vg.photogallery.controller.fragments.PhotoGalleryFragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
