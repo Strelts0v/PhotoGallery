93236bf3ea6417779fba08d4d34d68ce5a7e79a5
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-17 13:57:35 +0300

    update class QueryPreferences

--- app/src/main/java/com/vg/photogallery/util/QueryPreferences.java
+++ app/src/main/java/com/vg/photogallery/util/QueryPreferences.java
@@ -12,6 +12,8 @@ public class QueryPreferences {
 
     private static final String PREF_LAST_RESULT_ID = "lastResultId";
 
+    private static final String PREF_IS_ALARM_ON = "isAlarmOn";
+
     public static String getStoredQuery(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(PREF_SEARCH_QUERY, null);
@@ -33,6 +35,18 @@ public class QueryPreferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putString(PREF_LAST_RESULT_ID, lastResultId)
+                .apply();
+    }
+
+    public static boolean isAlarmOn(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(PREF_IS_ALARM_ON, false);
+    }
+
+    public static void setAlarmOn(Context context, boolean isOn) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PREF_IS_ALARM_ON, isOn)
                 .apply();
     }
 }
